2c7114ca1498535a7df29ee20acc3aa1a769e6be
Алексеев Юрий <aleksjurii@gmail.com>
2017-01-25 00:26:00 +0200

    24.01.17

--- password.php
+++ password.php
@@ -12,119 +12,125 @@ require_once SYS_DIR . 'core' . DS . 'boot.php';
 	<meta http-equiv = "Content-Language" content = "ru"/>
 </head>
 <style>
-    body {
-        padding: 0;
-        margin: 0;
-        text-align: center;
-        /* font-size: 14px; */
-        /* font-family: 'Roboto', sans-serif; */
-        background-color: #e4e5e5;
-        font-family: Verdana, sans-serif;
-        font-size: 13px;
-        font-weight: 400;
-        font-style: normal;
-        color: #000000;
-        line-height: 1.53em;
-        letter-spacing: 0;
-    }
 
-    h1 {
-        display: block;
-        font-weight: bold;
-        font-size: 24px;
-        color: #243541;
-        margin: 24px 0 17px;
-    }
+     body {
+         padding: 0;
+         margin: 0;
+         text-align: center;
+         /*font-size: 14px;*/
+         /*font-family: 'Roboto', sans-serif;*/
+         background-color: #e4e5e5;
 
-    form {
-        display: inline-block;
-        background-color: #f7f7f7;
-        padding: 22px 25px;
-        border: 1px solid #56b9ff;
-        margin-bottom: 15px;
-        width: 300px;
-    }
+         font-family: Verdana, sans-serif;
+         font-size: 13px;
+         font-weight: 400;
+         font-style: normal;
+         color: #000000;
+         line-height: 1.53em;
+         letter-spacing: 0;
 
-    .form_group input {
-        height: 24px;
-        width: 180px;
-        padding: 0 5px;
-        background: #fff;
-        border: 1px solid #d0d0d0;
-    }
+     }
+     h1, h2 {
+         display: block;
+         font-weight: bold;
+         color: #243541;
+     }
+     h1 {
+         font-size: 24px;
+         margin: 34px 0 25px;
+     }
+     h2 {
+         font-size: 20px;
+         color: #243541;
+         margin: 30px 0 25px;
+     }
 
-    .form_group {
-        text-align: left;
-        margin-bottom: 12px;
-    }
+     #system_logo {
+         height: 120px;
+         background-color: #091A33;
+     }
 
-    .form_group label {
-        display: inline-block;
-        width: 110px;
-        font-weight: 300;
-    }
-
-    label {
-        cursor: default;
-    }
+     form {
+         display: inline-block;
+         background-color: #f7f7f7;
+         padding: 22px 25px;
+         border: 1px solid #56b9ff;
+         margin-bottom: 15px;
+         /*width: 300px;*/
+     }
+     .form_group {
+         text-align: left;
+         margin-bottom: 12px;
+     }
+     .form_group label {
+         display: inline-block;
+         width: auto;
+         margin-right: 20px;
+         font-weight: 300;
+         float: right;
+     }
+     .form_group input {
+         height: 24px;
+         width: 100%;
+         padding: 0 5px;
+         background: #fff;
+         border: 1px solid #d0d0d0;
+     }
+     input:focus {
+         outline: none;
+     }
+     .button {
+         background: #ffcc00;
+         margin-top: 10px;
+         border: none;
+         border-radius: 2px;
+         padding: 9px 20px;
+         font-size: 16px;
+         color:#353b3e;
+         cursor: pointer;
+         -webkit-transition: all 0.3s ease;
+         -moz-transition: all 0.3s ease;
+         -o-transition: all 0.3s ease;
+         transition: all 0.3s ease;
+     }
+     .button:hover {
+         color: #fff;
+         background: #56b9ff;
+     }
+     .message_error {
+         background-color: #a70606;
+         padding: 12px;
+         color: #fff;
+         margin-bottom: 20px;
+     }
+     .recovery {
+         color: #243541;
+         margin-right: 5px;
+     }
 
     p {
         font-size: 19px;
     }
-
-    input {
-        font-size: 18px;
-    }
-
-    .button {
-        background: #ffcc00;
-        margin-top: 10px;
-        border: none;
-        border-radius: 2px;
-        padding: 9px 20px;
-        font-size: 16px;
-        color: #353b3e;
-        cursor: pointer;
-        -webkit-transition: all 0.3s ease;
-        -moz-transition: all 0.3s ease;
-        -o-transition: all 0.3s ease;
-        transition: all 0.3s ease;
-    }
-
-    p.error {
-        color: red;
-    }
-
-    div.maindiv {
-        width: 600px;
-        height: 300px;
-        position: relative;
-        left: 50%;
-        top: 0px;
-        margin-left: -300px;
-    }
 </style>
+
 <body>
 <div style = 'width:100%; height:100%;'>
-  <div class = "maindiv">
 
 <?php
-
-
 $registry = new Registry();
 
 // Если пришли по ссылке из письма
-if (!$c = $registry->request->get('code')){
+if ($c = $registry->request->get('code')){
     // Код не совпадает - прекращяем работу
     if (empty($_SESSION['admin_password_recovery_code']) || empty($c) || $_SESSION['admin_password_recovery_code'] !== $c){
-        //   header('Location:password.php');
-        //   exit();
+           header('Location:password.php');
+           exit();
     }
 
     // IP не совпадает - прекращяем работу
     if (empty($_SESSION['admin_password_recovery_ip']) || empty($_SERVER['REMOTE_ADDR']) || $_SESSION['admin_password_recovery_ip'] !== $_SERVER['REMOTE_ADDR']){
-        //   header('Location:password.php');
-        //   exit();
+           header('Location:password.php');
+           exit();
     }
 
     // Если запостили пароль
@@ -140,13 +146,18 @@ if (!$c = $registry->request->get('code')){
             $registry->managers->add_manager(['login' => $new_login, 'password' => $new_password]);
         }
 
-        print "
+        print '
+        <div id="system_logo">
+            <img src="backend/design/images/system_logo.png" alt="AlexShop CMS">
+        </div>
             <h1>Восстановление пароля администратора</h1>
             <p>
             Новый пароль установлен
             </p>
             <p>
-            <a href='" . $registry->root_url . "/backend/index.php?module=AuthAdmin'>Перейти в панель управления</a>";
+            <a href="' . $registry->root_url . '/backend/index.php?module=AuthAdmin">Перейти в панель входа</a>
+            </p>
+            ';
     } else {
         // Форма указания нового логина и пароля
         print '
@@ -163,23 +174,28 @@ if (!$c = $registry->request->get('code')){
                 <label>Новый пароль:</label>
             	<input type="password" name="new_password">
              </div> 
+             <a class="recovery" href="' . $registry->root_url . '/backend/index.php?module=AuthAdmin">Перейти в панель входа</a>
             	<input class="button" type="submit" value="Сохранить логин и пароль"> 
             </form>
         ';
     }
 } else {
+    // восстановление пароля по email
     print '
+        <div id="system_logo">
+            <img src="backend/design/images/system_logo.png" alt="AlexShop CMS">
+        </div>
         <h1>Восстановление пароля администратора</h1>
         <p>
-            Введите email администратора
+            <h2>Введите email администратора</h2>
             <form method="post" action="' . $registry->config->root_url . '/password.php" >
             <div class="form_group">
-                <label>Логин:</label>
+                <label>Email:</label>
             	<input type="text" name="email">
              </div> 
-             <div class="form_group">   
-            	<input type="submit" value="Восстановить пароль">
-             </div> 	
+             <a class="recovery" href="' . $registry->root_url . '/backend/index.php?module=AuthAdmin">Перейти в панель входа</a>
+            	<input class="button" type="submit" value="Восстановить пароль">
+             	
             </form>
         </p>
     ';
@@ -203,7 +219,7 @@ if (!$c = $registry->request->get('code')){
 }
 ?>
 
-  </div>
+
 </div>
 </body>
 </html>
