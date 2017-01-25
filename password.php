9f0d5b3ce0edf0caa448cbe48da8666267613ad9
Алексеев Юрий <aleksjurii@gmail.com>
2017-01-25 04:08:26 +0200

    25.01.17

--- password.php
+++ password.php
@@ -1,15 +1,14 @@
 <?php
 use api\Registry;
 
+require_once __DIR__ . '/system/configs/define/config.php';
+require_once SYS_DIR . 'core' . DS . 'boot.php';
+
 if (!empty($_SERVER['HTTP_USER_AGENT'])){
     session_name(md5($_SERVER['HTTP_USER_AGENT']));
 }
-session_start();
 
-require_once __DIR__ . '/system/configs/define/config.php';
-require_once SYS_DIR . 'core' . DS . 'boot.php';
-?>
-
+$head = '
 <html>
 <head>
     <meta http-equiv = "Content-Type" content = "text/html; charset=UTF-8"/>
@@ -22,42 +21,43 @@ require_once SYS_DIR . 'core' . DS . 'boot.php';
     <link href = "/backend/design/css/auth.css" rel = "stylesheet" type = "text/css"/>
 </head>
 <body>
-<div style = 'width:100%; height:100%;'>
+<div style = "width:100%; height:100%;">
+<div id="system_logo">
+<img src="backend/design/images/system_logo.png" alt="AlexShop CMS">
+</div>
+';
 
-<?php
+
 $registry = new Registry();
 
 // Если пришли по ссылке из письма
-if ($c = $registry->request->get('code')){
+$c = $registry->request->get('code');
+if ($c){
     // Код не совпадает - прекращяем работу
     if (empty($_SESSION['admin_password_recovery_code']) || empty($c) || $_SESSION['admin_password_recovery_code'] !== $c){
-        header('Location:password.php');
+        header('Location:admin');
         exit();
     }
 
     // IP не совпадает - прекращяем работу
     if (empty($_SESSION['admin_password_recovery_ip']) || empty($_SERVER['REMOTE_ADDR']) || $_SESSION['admin_password_recovery_ip'] !== $_SERVER['REMOTE_ADDR']){
-        header('Location:password.php');
+        header('Location:admin');
         exit();
     }
 
-    // Если запостили пароль
+    // Если запостили логин и пароль
     if ($new_password = $registry->request->post('new_password')){
-        // Удаляем из сесси код, чтобы больше никто не воспользовался ссылкой
-        unset($_SESSION['admin_password_recovery_code'], $_SESSION['admin_password_recovery_ip']);
-
-        // Новый логин и пароль
+        // Новый логин
         $new_login = $registry->request->post('new_login');
-        $new_password = $registry->request->post('new_password');
         $manager = $registry->managers->get_manager($new_login);
         if (!$registry->managers->update_manager($manager->id, ['password' => $new_password, 'cnt_try' => 0, 'last_try' => null])){
             $registry->managers->add_manager(['login' => $new_login, 'password' => $new_password]);
         }
+        // Удаляем из сессии код, чтобы больше никто не воспользовался ссылкой
+        unset($_SESSION['admin_password_recovery_code'], $_SESSION['admin_password_recovery_ip']);
 
-        print '
-        <div id="system_logo">
-            <img src="backend/design/images/system_logo.png" alt="AlexShop CMS">
-        </div>
+        echo $head,
+            '
             <h1>Восстановление пароля администратора</h1>
             <p>
             Новый пароль установлен
@@ -68,10 +68,8 @@ if ($c = $registry->request->get('code')){
             ';
     } else {
         // Форма указания нового логина и пароля
-        print '
-        <div id="system_logo">
-            <img src="backend/design/images/system_logo.png" alt="AlexShop CMS">
-        </div>
+        echo $head,
+        '
             <h1>Восстановление пароля администратора</h1>
             <form method="post">
             <div class="form_group">
@@ -93,10 +91,8 @@ if ($c = $registry->request->get('code')){
     }
 } else {
     // восстановление пароля по email
-    print '
-        <div id="system_logo">
-            <img src="backend/design/images/system_logo.png" alt="AlexShop CMS">
-        </div>
+    echo $head,
+    '
         <h1>Восстановление пароля администратора</h1>
         <p>
             <form method="post" action="' . $registry->config->root_url . '/password.php" >
@@ -126,12 +122,12 @@ if ($c = $registry->request->get('code')){
             $message .= 'Если письмо пришло вам по ошибке, проигнорируйте его.';
 
             $registry->notify->email($admin_email, 'Восстановление пароля администратора ' . $registry->settings->site_name, $message, $registry->settings->notify_from_email);
-            print '
+            echo '
             <div class = "message_ok">
             Вам отправлена ссылка для восстановления пароля. Если письмо вам не пришло, значит вы неверно указали email или что-то не так с хостингом.
             </div>';
         } else {
-            print '
+            echo '
             <div class = "message_error">
                 Email не существует!
             </div>';
