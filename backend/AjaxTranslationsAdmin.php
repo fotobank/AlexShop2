b2073887946d0c19f7582270aa11f98690a85afa
Алексеев Юрий <aleksjurii@gmail.com>
2017-02-09 20:04:38 +0200

    9.02.17

--- backend/AjaxTranslationsAdmin.php
+++ backend/AjaxTranslationsAdmin.php
@@ -1,13 +1,21 @@
 <?php
 
 use api\Registry;
+use exception\CommonException;
 
+class AjaxTranslationsAdminException extends CommonException
+{
+}
+
+
+/**
+ * Class AjaxTranslationsAdmin
+ */
 class AjaxTranslationsAdmin extends Registry
 {
 
     public function fetch()
     {
-        $filter = [];
         $filter['langs'] = $this->design->get_var('langs_label');
 
         // Обработка действий
@@ -17,12 +25,12 @@ class AjaxTranslationsAdmin extends Registry
 
                 // ajax вывод заголовка jqGrid таблицы
                 if (!empty($this->request->post('jqgrid_heading', 'integer'))){
-                    $this->put_ajax_head($filter['langs']);
+                    $this->head_table($filter['langs']);
                 }
                 // ajax вывод jqGrid таблицы
                 if ($this->request->post('_search', 'string') == 'false'){
-                    $this->put_ajax_body($filter);
-                // поиск
+                    $this->body_table($filter);
+                    // поиск
                 } elseif ($this->request->post('_search', 'string') == 'true') {
                     $this->search($filter);
                 }
@@ -41,22 +49,101 @@ class AjaxTranslationsAdmin extends Registry
     }
 
     /**
-     *
+     * названия колонок в таблице
+     * @param $langs
      */
-    protected function delete() {
-        $id = $this->request->post('id', 'integer');
-        $this->languages->delete_translation($id);
+    protected function head_table($langs)
+    {
+        $lang_list = $this->languages->lang_list();
+        $head = ['id', 'переменная в шаблоне'];
+        foreach ($langs as $short_lang){
+            $head[] = mb_strtolower($lang_list["$short_lang"]->name);
+        }
+        $model = $this->model_table($langs);
 
-        $error = $this->db->getMysqli()->error;
-        $mess = ($error != '') ? ['message' => 'Error: ' . $error] : ['message' => 'Success: запись удалена'];
+        $json = json_encode(['head' => $head, 'model' => $model]);
+        $this->response($json);
+    }
 
+    /**
+     * разметка таблицы
+     *
+     * @param $langs_label
+     *
+     * @return array
+     */
+    private function model_table($langs_label)
+    {
+        $model[] = [
+            'name' => 'id', 'index' => 'id', 'readOnly' => true, 'width' => 15, 'editable' => false, 'search' => false
+        ];
+        $model[] = ['name' => 'label', 'index' => 'label', 'searchrules' => ['required' => true], 'editable' => true, 'edittype' => 'textarea', 'width' => 80];
+        foreach ($langs_label as $lang){
+            $model[] = ['name' => "lang_$lang", 'index' => "lang_$lang", 'searchrules' => ['required' => true], 'editable' => true, 'edittype' => 'textarea', 'width' => 80];
+        };
+
+        return $model;
+    }
+
+    /**
+     * вывод результата
+     *
+     * @param $json
+     */
+    protected function response($json)
+    {
         header('Content-type: application/json; charset=UTF-8');
         header('Cache-Control: must-revalidate');
         header('Pragma: no-cache');
         header('Expires: -1');
-        $json = json_encode($mess);
         print $json;
         exit();
+    }
+
+    /**
+     * @param $filter
+     */
+    protected function body_table($filter)
+    {
+        // Получаем номер страницы. Сначала jqGrid ставит его в 1.
+        $filter['page'] = $this->request->post('page', 'integer');
+
+        // сколько строк мы хотим иметь в таблице - rowNum параметр
+        $filter['limit'] = $this->request->post('rows', 'integer');
+
+        // Колонка для сортировки. Сначала sortname параметр
+        // затем index из colModel
+        $filter['sidx'] = $this->request->post('sidx', 'string');
+
+        // Порядок сортировки.
+        $filter['sord'] = $this->request->post('sord', 'string');
+
+        // Если колонка сортировки не указана, то будем
+        // сортировать по первой колонке.
+        if (!$filter['sidx']) $filter['sidx'] = 1;
+
+        // Вычисляем количество строк для навигации..
+        $count = $this->languages->get_count();
+
+        // Вычисляем общее количество страниц.
+        if ($count > 0 && $filter['limit'] > 0){
+            $total_pages = ceil($count / $filter['limit']);
+        } else {
+            $total_pages = 0;
+        }
+        // Если запрашиваемый номер страницы больше общего количества страниц,
+        // то устанавливаем номер страницы в максимальный.
+        if ($filter['page'] > $total_pages) $filter['page'] = $total_pages;
+
+        // Вычисляем начальное смещение строк.
+        $filter['start'] = $filter['limit'] * $filter['page'] - $filter['limit'];
+
+        // Если начальное смещение отрицательно, то устанавливаем его в 0.
+        // Например, когда пользователь выбрал 0 в качестве запрашиваемой страницы.
+        if ($filter['start'] < 0) $filter['start'] = 0;
+
+        $json = json_encode($this->languages->get_translations($filter));
+        $this->response($json);
     }
 
     /**
@@ -71,63 +158,58 @@ class AjaxTranslationsAdmin extends Registry
             $sortingField = $this->request->post('sidx', 'string');
             $sortingOrder = $this->request->post('sord', 'string');
 
-            $qWhere = '';
-            if (isset($_POST['_search']) && $_POST['_search'] == 'true'){
 
-                $allowedFields = ['id', 'label'];
-                foreach ($filter['langs'] as $short_lang){
-                    $allowedFields[] = 'lang_' . $short_lang;
-                }
+            $allowedFields = ['id', 'label'];
+            foreach ($filter['langs'] as $short_lang){
+                $allowedFields[] = 'lang_' . $short_lang;
+            }
 
-                $allowedOperations = ['AND', 'OR'];
+            $allowedOperations = ['AND', 'OR'];
 
-                $searchData = json_decode($_POST['filters']);
+            $searchData = json_decode($_POST['filters']);
 
-                //ограничение на количество условий
-                if (count($searchData->rules) > 10){
-                    throw new Exception('Cool hacker is here!!! :)');
-                }
+            //ограничение на количество условий
+            if (count($searchData->rules) > 10){
+                throw new AjaxTranslationsAdminException('Cool hacker is here!!! :)');
+            }
 
-                $qWhere = ' WHERE ';
-                $firstElem = true;
-                //объединяем все полученные условия
-                foreach ($searchData->rules as $rule){
+            $qWhere = ' WHERE ';
+            $firstElem = true;
+            //объединяем все полученные условия
+            foreach ($searchData->rules as $rule){
 
-                    if (!$firstElem) {
+                if (!$firstElem){
                     //объединяем условия (с помощью AND или OR)
                     if (in_array($searchData->groupOp, $allowedOperations)){
                         $qWhere .= ' ' . $searchData->groupOp . ' ';
                     } else {
                         //если получили не существующее условие - возвращаем описание ошибки
-                        throw new Exception('Cool hacker is here!!! :)');
+                        throw new AjaxTranslationsAdminException('Cool hacker is here!!! :)');
                     }
+                } else {
+                    $firstElem = false;
+                }
+                //вставляем условия
+                if (in_array($rule->field, $allowedFields)){
+                    switch ($rule->op) {
+                        case 'eq': // равно
+                            $qWhere .= $rule->field . " = '" . $this->db->escape($rule->data) . "'";
+                            break;
+                        case 'ne': // не равно
+                            $qWhere .= $rule->field . " <> '" . $this->db->escape($rule->data) . "'";
+                            break;
+                        case 'bw': // начинаетя с
+                            $qWhere .= $rule->field . " LIKE '" . $this->db->escape($rule->data . '%') . "'";
+                            break;
+                        case 'cn': // содержит
+                            $qWhere .= $rule->field . " LIKE '" . $this->db->escape('%' . $rule->data . '%') . "'";
+                            break;
+                        default:
+                            throw new AjaxTranslationsAdminException('Cool hacker is here!!! :)');
                     }
-                    else {
-                        $firstElem = false;
-                    }
-                    //вставляем условия
-                    if (in_array($rule->field, $allowedFields)){
-                        switch ($rule->op) {
-                            case 'eq': // равно
-                                $qWhere .= $rule->field . " = '" . $this->db->escape($rule->data)."'";
-                                break;
-                            case 'ne': // не равно
-                                $qWhere .= $rule->field . " <> '" . $this->db->escape($rule->data)."'";
-                                break;
-                            case 'bw': // начинаетя с
-                                $qWhere .= $rule->field . " LIKE '" . $this->db->escape($rule->data . '%')."'";
-                                break;
-                            case 'cn': // содержит
-                                $qWhere .= $rule->field . " LIKE '" . $this->db->escape('%' . $rule->data . '%')."'";
-                                break;
-                            default:
-                                throw new Exception('Cool hacker is here!!! :)');
-                        }
-                    } else {
-                        //если получили не существующее условие - возвращаем описание ошибки
-                        throw new Exception('Cool hacker is here!!! :)');
-                    }
-
+                } else {
+                    //если получили не существующее условие - возвращаем описание ошибки
+                    throw new AjaxTranslationsAdminException('Cool hacker is here!!! :)');
                 }
             }
 
@@ -142,7 +224,7 @@ class AjaxTranslationsAdmin extends Registry
             $translations = $this->db->results();
 
             //определяем количество записей в таблице
-            $totalRows =  $this->languages->get_count($qWhere);
+            $totalRows = $this->languages->get_count($qWhere);
 
             //сохраняем номер текущей страницы, общее количество страниц и общее количество записей
             $response = new stdClass();
@@ -151,7 +233,7 @@ class AjaxTranslationsAdmin extends Registry
             $response->records = $totalRows;
 
             $rows = [];
-            foreach ($translations as $key => $row) {
+            foreach ($translations as $key => $row){
                 $rows[$key]['id'] = $row->id;
                 $rows[$key]['cell'] = [$row->id, $row->label];
                 foreach ($filter['langs'] as $short_lang){
@@ -159,9 +241,13 @@ class AjaxTranslationsAdmin extends Registry
                 }
             }
             $response->rows = $rows;
-            echo json_encode($response);
-        } catch (Exception $e) {
-            echo json_encode(['errMess' => 'Error: ' . $e->getMessage()]);
+            $response = json_encode($response);
+            $this->response($response);
+
+        } catch (AjaxTranslationsAdminException $e) {
+
+            $json = json_encode(['errMess' => 'Error: ' . $e->getMessage()]);
+            $this->response($json);
         }
     }
 
@@ -208,109 +294,25 @@ class AjaxTranslationsAdmin extends Registry
             $this->languages->update_translation_config_js();
         }
 
-        $mess = ($error != '') ? ['message' => 'Error: ' . $error] : ['message' => 'Success: ' . $success];
+        $json = ($error != '') ? ['message' => 'Error: ' . $error] : ['message' => 'Success: ' . $success];
 
-        header('Content-type: application/json; charset=UTF-8');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: no-cache');
-        header('Expires: -1');
-        $json = json_encode($mess);
-        print $json;
-        exit();
+        $json = json_encode($json);
+        $this->response($json);
     }
 
     /**
-     * @param $filter
+     * удаление записи
      */
-    protected function put_ajax_body($filter)
+    protected function delete()
     {
-        // Получаем номер страницы. Сначала jqGrid ставит его в 1.
-        $filter['page'] = $this->request->post('page', 'integer');
+        $id = $this->request->post('id', 'integer');
+        $this->languages->delete_translation($id);
 
-        // сколько строк мы хотим иметь в таблице - rowNum параметр
-        $filter['limit'] = $this->request->post('rows', 'integer');
+        $error = $this->db->getMysqli()->error;
+        $json = ($error != '') ? ['message' => 'Error: ' . $error] : ['message' => 'Success: запись удалена'];
 
-        // Колонка для сортировки. Сначала sortname параметр
-        // затем index из colModel
-        $filter['sidx'] = $this->request->post('sidx', 'string');
-
-        // Порядок сортировки.
-        $filter['sord'] = $this->request->post('sord', 'string');
-
-        // Если колонка сортировки не указана, то будем
-        // сортировать по первой колонке.
-        if (!$filter['sidx']) $filter['sidx'] = 1;
-
-        // Вычисляем количество строк для навигации..
-        $count = $this->languages->get_count();
-
-        // Вычисляем общее количество страниц.
-        if ($count > 0 && $filter['limit'] > 0){
-            $total_pages = ceil($count / $filter['limit']);
-        } else {
-            $total_pages = 0;
-        }
-        // Если запрашиваемый номер страницы больше общего количества страниц,
-        // то устанавливаем номер страницы в максимальный.
-        if ($filter['page'] > $total_pages) $filter['page'] = $total_pages;
-
-        // Вычисляем начальное смещение строк.
-        $filter['start'] = $filter['limit'] * $filter['page'] - $filter['limit'];
-
-        // Если начальное смещение отрицательно, то устанавливаем его в 0.
-        // Например, когда пользователь выбрал 0 в качестве запрашиваемой страницы.
-        if ($filter['start'] < 0) $filter['start'] = 0;
-
-        $body = $this->languages->get_translations($filter);
-
-        header("Content-type: application/json; charset=UTF-8");
-        header("Cache-Control: must-revalidate");
-        header("Pragma: no-cache");
-        header("Expires: -1");
-        $json = json_encode($body);
-        print $json;
-        exit();
-    }
-
-    /**
-     * шапка таблицы
-     *
-     * @param $langs_label
-     *
-     * @return array
-     */
-    private function get_grid_model($langs_label)
-    {
-        $head[] = [
-            'name' => 'id', 'index' => 'id', 'readOnly' => true, 'width' => 15, 'editable' => false, 'search' => false
-        ];
-        $head[] = ['name' => 'label', 'index' => 'label', 'searchrules' => ['required' => true], 'editable' => true, 'edittype' => 'textarea', 'width' => 80];
-        foreach ($langs_label as $lang){
-            $head[] = ['name' => "lang_$lang", 'index' => "lang_$lang", 'searchrules' => ['required' => true], 'editable' => true, 'edittype' => 'textarea', 'width' => 80];
-        };
-
-        return $head;
-    }
-
-    /**
-     * @param $langs
-     */
-    protected function put_ajax_head($langs)
-    {
-        $lang_list = $this->languages->lang_list();
-        $head = ['id', 'переменная в шаблоне'];
-        foreach ($langs as $short_lang){
-            $head[] = mb_strtolower($lang_list["$short_lang"]->name);
-        }
-        $model = $this->get_grid_model($langs);
-
-        header("Content-type: application/json; charset=UTF-8");
-        header("Cache-Control: must-revalidate");
-        header("Pragma: no-cache");
-        header("Expires: -1");
-        $json = json_encode(['head' => $head, 'model' => $model]);
-        print $json;
-        exit();
+        $json = json_encode($json);
+        $this->response($json);
     }
 
 }
